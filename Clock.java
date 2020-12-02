febda517f4c32f61969cf83e6674343100083c66
Plichta <plichta@vdi-sccubu-047.lancs.local>
2020-12-02 01:13:48 +0000

    Added the Functionality for the Clock class - still need to implement variable clock times. The class uses and deprecated library, I am aware.

--- Clock.java
+++ Clock.java
@@ -1,3 +1,35 @@
-public class Clock {
+import java.util.Observable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
+/**
+ * This clock class implements a Singleton design pattern.
+ * To instantiate this clock class you MUST use the 'getInstance()' mothod.
+ * Observable is deprecated, however, it's useful for this application.
+ */
+public class Clock extends Observable implements ActionListener {
     
+    private static Clock instance = null;
+    private Timer time;
+
+    //This implements a Singleton design pattern
+    public static Clock getInstance() {
+        if(instance == null) 
+            instance = new Clock();
+
+        return instance;        
+    }
+
+    //Private constructor on purpose, DO NOT CHANGE!
+    private Clock() {
+        time = new Timer(1000, this);
+        time.start();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.setChanged();
+        this.notifyObservers();
+    }
 }
