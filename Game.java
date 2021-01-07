e5bebdc239b22a2be273b78e4761d23b620af61b
Daniel Lennon <d.lennonbeato@lancaster.ac.uk>
2021-01-07 21:33:01 +0000

    Added in a backround placeholder unitil a new backround is made. Also added code which allows to detect if users clicked on specific locations, this will allow for user events such as deciding where to plant seeds and what crops to unlock or even using the house as the in game store, for upgrades.

--- Game.java
+++ Game.java
@@ -12,59 +12,54 @@ import java.io.*;
 
 public class Game 
 {
-    int length = 640;
-    int width = 480;
-    int speed = 15; 
+    int heigth = 600;
+    int width = 800;
+    int speed = 10; 
     int speed2 = 10;
 
     RenderWindow window = new RenderWindow();
     
-    CircleShape circle = new CircleShape(15);
     Texture farmer = new Texture();
+    Texture backroundTexture = new Texture();
 
     Image farmerImage = new Image();
     Texture farmerTexture = new Texture();
 
-    Path path = FileSystems.getDefault().getPath("BoringGame", "Man_Neutral.png");
+    //Path path = FileSystems.getDefault().getPath("BoringGame", "Man_Neutral.png");
 
     Vector2f scale = new Vector2f((float)1.5, (float)1.5);
 
     Sprite farmerSprite = new Sprite();
+    RectangleShape backround = new RectangleShape();
 
     public Game()
     {
         //Create the window
-        window.create(new VideoMode(640, 480), "Hello JSFML!");
+        window.create(new VideoMode(width, heigth), "Escape from CommieLand!");
 
         //Limit the framerate
         window.setFramerateLimit(60);
 
-        try 
-        {
-            BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
-        } 
-        catch (Exception e) 
-        {
-            //TODO: handle exception
-        }
+        this.loadPathToSprite("BoringGame", "Man_Neutral.png", farmerSprite, farmerTexture);
+        this.loadPathToRectangle("BoringGame", "Forest.png", backround, backroundTexture);
 
-        try 
-        {
-            farmerTexture.loadFromFile(path);
-        } 
-        catch (Exception e) 
-        {
-            //TODO: handle exception
-        }
-
-        farmerSprite.setTexture(farmerTexture);
-        farmerSprite.setPosition(length/2, width/2);
+        //farmerSprite.setTexture(farmerTexture);
+        farmerSprite.setPosition(heigth/2, width/2);
         farmerSprite.setScale(scale);
         farmerSprite.scale(scale);
+
+        backround.setPosition(0, 0);
+        backround.setSize(new Vector2f(width, heigth));
     }
 
+
+    /*
+    Needs fixing in terms of bordering. Maybe combine getPosition() method with the getLocalBOunds() method to get an accurate location/position to allow delimiting to occur correctly.
+    */
     public void playGame()
     {
+        //System.out.println(farmerSprite.getScale() + "Hello there!");
+
         while(window.isOpen()) 
         {
             //Fill the window with red
@@ -72,8 +67,9 @@ public class Game
 
             if(Keyboard.isKeyPressed(Keyboard.Key.D))
             {
-                if(farmerSprite.getPosition().x /*+ circle.getRadius()*/ >= 640 - circle.getRadius()*2)
+                if(farmerSprite.getGlobalBounds().left + farmerSprite.getGlobalBounds().width >= width)
                 {
+                //System.out.println("I AM HERE " + farmerSprite.getPosition().x);
                 farmerSprite.move(0,0);
                 }
                 else
@@ -83,8 +79,9 @@ public class Game
             }
             else if(Keyboard.isKeyPressed(Keyboard.Key.A))
             {
-                if(farmerSprite.getPosition().x <= 0)
+                if(farmerSprite.getGlobalBounds().left <= 0)
                 {
+                //System.out.println("I AM HERE " + farmerSprite.getPosition().x);
                 farmerSprite.move(0,0);
                 }
                 else
@@ -94,18 +91,19 @@ public class Game
             }
             else if(Keyboard.isKeyPressed(Keyboard.Key.S))
             {
-                if(farmerSprite.getPosition().y >= 480 - circle.getRadius()*2)
+                if(farmerSprite.getGlobalBounds().top + farmerSprite.getGlobalBounds().height >= heigth)
                 {
                 farmerSprite.move(0,0);
                 }
                 else
                 {
+                //System.out.println("I AM HERE " + farmerSprite.getGlobalBounds().top + "&" + farmerSprite.getGlobalBounds().height);
                 farmerSprite.move(0,speed);
                 }
             }
             else if(Keyboard.isKeyPressed(Keyboard.Key.W))
             {
-                if(farmerSprite.getPosition().y <= 0)
+                if(farmerSprite.getGlobalBounds().top <= 0)
                 {
                 farmerSprite.move(0,0);
                 }
@@ -115,6 +113,19 @@ public class Game
                 }
             }
 
+            /*
+            Following code will be useful for idenfying when the player is clicking on a certain crop or on the house etc...
+            Right now doesnt do much but still a good starting point. 
+            */
+            if(Mouse.isButtonPressed(Mouse.Button.LEFT))
+            {
+                if(Mouse.getPosition(window).x >= width/3 && Mouse.getPosition(window).x <= 2 * width/3 && Mouse.getPosition().y >= heigth/3 && Mouse.getPosition().y <= 2 * heigth/3)
+                {
+                    System.out.println("Test successful!");
+                }
+            }
+
+            window.draw(backround);
             window.draw(farmerSprite);
             window.display();
 
@@ -128,30 +139,53 @@ public class Game
                 }
             }
         }
-    
+    }
 
-        /*
-        public loadPathToSprite(String directory, String file)
+    public void loadPathToSprite(String directory, String file, Sprite sprite, Texture texture)
+    {
+        Path path = FileSystems.getDefault().getPath(directory, file);
+        
+        try 
         {
-            Path path = FileSystems.getDefault().getPath(directory, file);
-            
-            try 
-            {
-                BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+            BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
 
-            } catch (Exception e) {
-                //TODO: handle exception
-            }
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
 
-            try 
-            {
-                farmerTexture.loadFromFile(path);
-            } 
-            catch (Exception e) 
-            {
-                //TODO: handle exception
-            }
-        }*/
+        try 
+        {
+            texture.loadFromFile(path);
+        } 
+        catch (Exception e) 
+        {
+            //TODO: handle exception
+        }
 
+        sprite.setTexture(texture);
+    }
+
+    public void loadPathToRectangle(String directory, String file, RectangleShape rectangle, Texture texture)
+    {
+        Path path = FileSystems.getDefault().getPath(directory, file);
+        
+        try 
+        {
+            BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        try 
+        {
+            texture.loadFromFile(path);
+        } 
+        catch (Exception e) 
+        {
+            //TODO: handle exception
+        }
+
+        rectangle.setTexture(texture);
     }
 }
